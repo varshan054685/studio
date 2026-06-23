4d84fbfd8f3f5fe070ad26e2a0efc14bee577228
Varshan <varshanrio05@gmail.com>
2026-06-23 07:36:24 +0530

    chore: remove unused documentation and mock data files

--- src/app/lib/mock-data.ts
+++
@@ -1,5 +0,0 @@
-import { Transaction, BudgetGoal } from './types';
-
-// Mock data is now empty to encourage real user input
-export const MOCK_TRANSACTIONS: Transaction[] = [];
-export const MOCK_BUDGETS: BudgetGoal[] = [];
